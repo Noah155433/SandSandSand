99eb45636fb58fdb95ee3b8071fb789e31d23cd7
Noah Ivarsson <noahivarsson9@gmail.com>
2025-12-30 14:28:58 +0100

    Feat: Initialized GLFW, window, GLL loader, and gl Viewport. Shaders, Vertex Buffer and Vertex Array, Input function, Rendering

--- Source.cpp
+++ Source.cpp
@@ -9,9 +9,82 @@
 #include "shader_s.h"
 
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <random>
 
-#include <Windows.h>
+void processInput(GLFWwindow* window);
 
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif // __APPLE__
+
+
+	GLFWwindow* window = glfwCreateWindow(2560, 1440, "SandSandSand", glfwGetPrimaryMonitor(), NULL);
+	if (window == NULL)
+	{
+		std::cout << "Window creation failed" << std::endl;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Could not load GLL loader" << std::endl;
+	}
+
+	glViewport(0, 0, 2560, 1440);
+
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+
+	Shader shader("VertexShader.vert", "FragmentShader.frag");
+
+	float cubeVertices[] =
+	{
+		-1.0f, -1.0f, 0.0f,
+		-1.0f,  1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		-1.0f,  1.0f, 0.0f,
+		 1.0f,  1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f
+	};
+
+	unsigned int VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+
+		processInput(window);
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shader.use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	}
+
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
