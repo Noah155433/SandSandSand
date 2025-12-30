5b73369327f47168eca63bc84483c16fc0994df9
Noah Ivarsson <noahivarsson9@gmail.com>
2025-12-30 13:40:36 +0100

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "stb_image.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "shader_s.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+
+#include <Windows.h>
+
